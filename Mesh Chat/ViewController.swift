8f15f51dd94cd0392582ea2cab9f5cecd4985742
Scott Chow <tehchowster@gmail.com>
2019-01-22 17:15:25 -0800

    Initial Commit

---
+++ Mesh Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mesh Chat
+//
+//  Created by Scott P. Chow on 1/22/19.
+//  Copyright © 2019 cs176b. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Mesh ChatTests/Mesh_ChatTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mesh_ChatTests.swift
+//  Mesh ChatTests
+//
+//  Created by Scott P. Chow on 1/22/19.
+//  Copyright © 2019 cs176b. All rights reserved.
+//
+
+import XCTest
+@testable import Mesh_Chat
+
+class Mesh_ChatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
