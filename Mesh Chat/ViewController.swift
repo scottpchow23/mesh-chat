3c5591056011803ac696eec4fc551c7c411c7b0f
kheff16 <kheffernan@umail.ucsb.edu>
2019-01-28 12:21:32 -0800

    First Page UI

--- Mesh Chat/ViewController.swift
+++ Mesh Chat/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func ClickChat(_ sender: Any) {
+    } // When the button is clicked we want to save the username as a variable. if the textfield is not changed do not allow a transition
+    
+    @IBAction func EnterUsername(_ sender: Any) {
+    } // Want to clear the text field when the user clicks on it. (Touch up Inside)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
