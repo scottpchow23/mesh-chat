48851c815406ee8f4cf45cee32e63595955a55c2
kheff16 <kheffernan@umail.ucsb.edu>
2019-02-05 11:46:18 -0800

    Added UUID pod and fixed MessageKit.

--- Mesh Chat/StartViewController.swift
+++ Mesh Chat/StartViewController.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class StartViewController: UIViewController {
     // MARK: Properties
-    
+    var firstTouch: Bool = true
     @IBOutlet weak var UsernameTextField: UITextField!
-    @IBOutlet weak var chatButton: UIButton!
-    @IBOutlet weak var test: UITextField!
-    
-    // MARK: UITextField Delegate
     
     // MARK: Actions
     
@@ -25,44 +21,12 @@ class StartViewController: UIViewController {
      Postcondition: Clear Textfield
      */
     @IBAction func clearField(_ sender: UITextField) {
-        UsernameTextField.text = ""; // Success
-    } // Want to clear the text field when the user clicks on it. (Touch Down)
-    
-    // MARK: UIButton Delegate
-    
-    // MARK: Actions
-    @IBAction func usernameInput(_ sender: Any) {
-        test.text = UsernameTextField.text
-    }
-    
-    /*
-     Precondition: Button Clicked
-     
-     Postcondition:  Username shown in test textfield and transition to tableview
-     */
-    @IBAction func ClickChat(_ sender: Any) {
-        if(UsernameTextField.text == "Enter a Username")
-        {
-            return // Not a proper username (should display a message)
+        if(firstTouch){
+            UsernameTextField.text = ""; // Success
         }
-        
-        // Not sure what to do for this
-        
-//        guard let username = UsernameTextField.text
-//            else
-//            {
-//                print("Something went wrong")
-//                return
-//            }
-//        test.text = username
-        return
-    }
-    
-    /*
-     Precondition: App Initialized
-     
-     Postcondition: View loaded
-     */
+        firstTouch = false
+    } // Want to clear the text field when the user clicks on it. (Touch Down)
+
 }
 
 
